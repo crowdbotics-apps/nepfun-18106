5e8a2d493b4e281216d911e812fa5f0beb4b5810
Crowdbotics <team@crowdbotics.com>
2020-06-14 22:44:32 +0000

    Update models

--- backend/nepfun_18106/wsgi.py
+++ backend/nepfun_18106/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nepfun_18106.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nepfun_18106.settings")
 
 application = get_wsgi_application()
